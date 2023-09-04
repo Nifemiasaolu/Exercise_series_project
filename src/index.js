d781f7dee3080fac19306dd4c51b62bb3b4f266b
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-09-04 14:36:31 +0100

    Compiled script

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './Exercise/Accordion Exercise/App';
+import App from "./Challenge/TipCalculator/App";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
