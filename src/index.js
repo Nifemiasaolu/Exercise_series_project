f5dec268b2f724ca4c784b05c81d8416a08af956
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-09-08 21:38:41 +0100

    Parent compiled function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 // import App from './Exercise/Accordion Exercise/App';
-import App from "./Challenge/TipCalculator/App";
+// import App from "./Challenge/TipCalculator/App";
+import App from "./Challenge/TextExpander/App";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
