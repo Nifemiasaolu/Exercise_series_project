3173029ec45542163e69b618adb03a8eb05a4a0c
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-09-19 20:35:30 +0100

    Parent component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './Exercise/Accordion Exercise/App';
 // import App from "./Challenge/TipCalculator/App";
-import App from "./Challenge/TextExpander/App";
+// import App from "./Challenge/TextExpander/App";
+// import App from './Exercise/Currency Converter/App';
+import App from './Challenge/useGeolocation/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
