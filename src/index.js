fa81f9e86cc668ac3bd2924f46786a921a6535e2
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-10-22 20:42:28 +0100

    Index tweak

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import ReactDOM from 'react-dom/client';
 // import App from "./Challenge/TextExpander/App";
 // import App from './Exercise/Currency Converter/App';
 // import App from './Challenge/useGeolocation/App';
-import App from './Exercise/Bank Account (useReducer)/App';
+// import App from './Exercise/Bank Account (useReducer)/App';
+// import App from "./Exercise/Advanced React Pattern/App";
+import App from "./Exercise/Compound Component Pattern/App";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
